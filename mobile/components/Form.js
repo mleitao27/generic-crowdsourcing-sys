f82816a8e0fdac3b1009b51d8308a44bc9c01357
Miguel Leitão <mleitao27@gmail.com>
2020-04-01 17:40:11 +0100

    Mobile: small json fixes.

--- mobile/components/Form.js
+++ mobile/components/Form.js
@@ -30,7 +30,7 @@ const Form = props => {
          if (e.type === 'text') form.push(<TextElement key={index} onChange={onChange} index={index} title={e.name} />);
         else if (e.type === 'boolean') form.push(<BooleanElement key={index} onChange={onChange} index={index} title={e.name} />);
         else if (e.type === 'checkbox') form.push(<CheckboxElement key={index} onChange={onChange} index={index} title={e.name} items={e.items} />);
-        else if (e.type === 'picker') form.push(<DropdownElement key={index} onChange={onChange} index={index} items={e.items} />);
+        else if (e.type === 'dropdown') form.push(<DropdownElement key={index} onChange={onChange} index={index} items={e.items} />);
         else if (e.type === 'radio') form.push(<RadioElement key={index} onChange={onChange} index={index} items={e.items} />);
         else if (e.type === 'range') form.push(<RangeElement key={index} onChange={onChange} index={index} min={e.min} max={e.max} />);
         else if (e.type === 'imagepicker') form.push(<ImagePickerElement key={index} onChange={onChange} index={index} items={e.items} />);
--- mobile/extension/config.js
+++ mobile/extension/config.js
@@ -9,7 +9,7 @@ const config = {
             appId: "534270243959641"
         }
     },
-    serverURL: 'http://192.168.0.101:3000'
+    serverURL: 'http://192.168.1.100:3000'
 };
 
 export default config;
