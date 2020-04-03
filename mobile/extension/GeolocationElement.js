eaabd2dc58c61ec6db122bc8273820d7c4bed9ce
Miguel Leitão <mleitao27@gmail.com>
2020-04-03 14:04:02 +0100

    Mobile: Changed extensio test element.

---
+++ mobile/extension/GeolocationElement.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { View, Text } from 'react-native';
+
+const GeolocationElement = props => {
+
+    return (
+        <View>
+            <Text>GeolocationElement still under construction...</Text>
+        </View>
+    );
+};
+
+export default GeolocationElement;
