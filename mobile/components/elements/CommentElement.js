16548b1c8248aaf80a49e6d451e3f9190288e438
Miguel Leitão <mleitao27@gmail.com>
2020-04-08 17:25:02 +0100

    Forms: Fix on boolean and checkbox. Comment done.

--- mobile/components/elements/CommentElement.js
+++ mobile/components/elements/CommentElement.js
@@ -1,14 +1,71 @@
-import React from 'react';
+// Imports
+import React, { useState, useEffect } from 'react';
+import {
+    View,
+    Text,
+    TextInput,
+    Dimensions,
+    StyleSheet
+} from 'react-native';
 
-import { View, Text } from 'react-native';
-
+// Comment box built with a large TextInput element
 const CommentElement = props => {
 
+    // Stores input value
+    const [value, setValue] = useState('');
+
+    // Submits empty string as initial answer
+    useEffect(() => {
+        // Send data through the onChange prop
+        props.onChange(props.pageIndex, props.index, '');
+    }, []);
+
+    // Handles new text input
+    const inputHandler = enteredValue => {
+        // Stores new value in state
+        setValue(enteredValue);
+        // Updates answer in Form component
+        props.onChange(props.pageIndex, props.index, enteredValue);
+    };
+
     return (
-        <View>
-            <Text>CommentElement still under construction...</Text>
+        <View style={styles.container}>
+            <View style={styles.headerContainer}>
+                <Text style={styles.title}>{props.title} </Text>
+                <Text style={styles.subTitle}>(max. 240)</Text>
+            </View>
+            <TextInput
+                style={styles.input}
+                onChangeText={inputHandler}
+                value={value}
+                multiline={true}
+                numberOfLines={6}
+                maxLength={240}
+            />
         </View>
     );
 };
+
+// Styles
+const styles = StyleSheet.create({
+    container: {
+        paddingVertical: Dimensions.get('window').height * 0.05
+    },
+    headerContainer: {
+        flexDirection: 'row',
+        marginBottom: Dimensions.get('window').height * 0.02
+    },
+    title: {
+        fontSize: 18
+    },
+    subTitle: {
+        fontSize: 16
+    },
+    input: {
+        backgroundColor: 'white',
+        textAlignVertical: 'top',
+        fontSize: 16
+    }
+});
 
 export default CommentElement;
