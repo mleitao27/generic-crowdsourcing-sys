6e23d0d42e859b9d1754f489f839279780657602
Miguel Leitão <mleitao27@gmail.com>
2020-04-12 03:11:38 +0100

    Forms: files recovery.

--- mobile/components/screens/FormScreen.js
+++ mobile/components/screens/FormScreen.js
@@ -1,32 +1,44 @@
 import React from 'react';
-import { View } from 'react-native';
+import { ScrollView } from 'react-native';
+import * as DocumentPicker from 'expo-document-picker';
 
-import globalStyles from '../../constants/globalStyles';
 import Colors from '../../constants/colors';
-import data from '../../data/surveyjs.json';
+import data from '../../data/form.json';
 
 import CustomButton from '../CustomButton';
 import Form from '../Form';
 
 import FormExtension from '../../extension/FormExtension';
 
+
 const FormScreen = props => {
+
+    const getFile = async () => {
+        const file = await DocumentPicker.getDocumentAsync();
+        console.log(file);
+    };
 
     const onSubmit = (data) => {
         console.log(data);
     };
 
     return (
-        <View style={{width:'100%'}}>
-            <Form json={data} extension={FormExtension} onSubmit={onSubmit} />
+        <ScrollView style={{ width: '100%' }}>
 
+            <Form json={data} extension={FormExtension} onSubmit={onSubmit} />
             <CustomButton
                 title='Results'
-                onPress={() => props.navigation.navigate({routeName: 'Results'})}
+                onPress={() => props.navigation.navigate({ routeName: 'Results' })}
                 backgroundColor={Colors.primary}
                 textColor={Colors.secondary}
             />
-        </View>
+            <CustomButton
+                title='File'
+                onPress={getFile}
+                backgroundColor={Colors.primary}
+                textColor={Colors.secondary}
+            />
+        </ScrollView>
     );
 };
 
