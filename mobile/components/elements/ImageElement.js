56471470df829d973831a438144ee73ff675972d
Miguel Leitão <mleitao27@gmail.com>
2020-04-13 01:51:31 +0100

    Forms: Image element.

--- mobile/components/elements/ImageElement.js
+++ mobile/components/elements/ImageElement.js
@@ -1,12 +1,43 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import {
+    View,
+    Image,
+    Text,
+    StyleSheet,
+    Dimensions
+} from 'react-native';
 
 const ImageElement = props => {
+    
+    const [width, setWidth] = useState(0);
+    const [height, setHeight] = useState(0);
+
+    useEffect(() => {
+        Image.getSize(props.url, (width, height) => {setHeight(height); setWidth(width);});
+    });
+
     return (
-        <View>
-            <Text>ImageElement still under construction...</Text>
+        <View style={styles.container}>
+            <Text style={styles.title}>{props.title}</Text>
+            <View style={styles.imageContainer}>
+                <Image source={{ uri: props.url }} style={{width, height}}/>
+            </View>
         </View>
     );
 };
+
+const styles = StyleSheet.create({
+    container: {
+        paddingVertical: Dimensions.get('window').height * 0.05
+    },
+    title: {
+        fontSize: 18,
+        marginBottom: Dimensions.get('window').height * 0.02
+    },
+    imageContainer: {
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+});
 
 export default ImageElement;
