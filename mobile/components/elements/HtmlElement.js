0b1da15903d0b4bb605ed2e91e6bc2a6dbffde4e
Miguel Leitão <mleitao27@gmail.com>
2020-04-13 23:22:12 +0100

    Forms: Html modal.

--- mobile/components/elements/HtmlElement.js
+++ mobile/components/elements/HtmlElement.js
@@ -1,12 +1,14 @@
 // Imports
-import React from 'react';
+import React, { useState } from 'react';
 import {
     View,
     Text,
     StyleSheet,
-    Dimensions
+    Dimensions,
+    TouchableOpacity,
+    Modal
 } from 'react-native';
-
+import Icon from "react-native-vector-icons/Ionicons";
 import { WebView } from 'react-native-webview';
 
 import Colors from '../constants/colors';
@@ -14,18 +16,43 @@ import Colors from '../constants/colors';
 // Displays expression inside a box in the form screen
 const HtmlElement = props => {
 
-    return (
-        <View style={styles.container}>
-            <Text style={styles.title}>{props.title}</Text>
-            <View style={styles.htmlContainer}>
-            <WebView
-                originWhitelist={['*']}
-                source={{html:props.html}}
-                style={styles.html}
-            />
+    // State to open modal with full scrollable html from webview
+    const [focused, setFocused] = useState(false);
+
+    // Display fixed height webview with html in form page
+    if (!focused)
+        return (
+            <View style={styles.container}>
+                <Text style={styles.title}>{props.title}</Text>
+                <TouchableOpacity style={styles.htmlContainer} onPress={() => setFocused(true)}>
+                    <WebView
+                        originWhitelist={['*']}
+                        source={{html:props.html}}
+                        style={styles.html}
+                    />
+                </TouchableOpacity>
             </View>
-        </View>
-    );
+        );
+    // Show entire html
+    else
+        return (
+            <Modal transparent={true}>
+                <View style={styles.modalBackground}>
+                    <View style={styles.modalContainer}>
+                        <View style={styles.modalHeader}>
+                            <TouchableOpacity onPress={() => setFocused(false)}>
+                                <Icon name='ios-close' size={56} color={Colors.primary} />
+                            </TouchableOpacity>
+                        </View>
+                        <WebView
+                            originWhitelist={['*']}
+                            source={{html:props.html}}
+                            style={styles.html}
+                        />
+                    </View>
+                </View>
+            </Modal>
+        );
 };
 
 // Styles
@@ -50,6 +77,25 @@ const styles = StyleSheet.create({
     htmlContainer: {
         borderWidth: 1,
         borderColor: Colors.secondary
+    },
+    modalBackground: {
+        flex: 1,
+        backgroundColor: 'rgba(0,0,0,0.5)',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    modalContainer: {
+        backgroundColor: 'white',
+        width: Dimensions.get('window').width*0.8,
+        height: Dimensions.get('window').height*0.8,
+        borderRadius: 20,
+        overflow: 'hidden'
+    },
+    modalHeader: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'flex-end',
+        paddingHorizontal: Dimensions.get('window').width*0.05
     }
 });
 
