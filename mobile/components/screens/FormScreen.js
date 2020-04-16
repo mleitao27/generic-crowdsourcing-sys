3267baef8e19b232f24f8ffee6765d737a730fda
Miguel Leitão <mleitao27@gmail.com>
2020-04-16 20:00:48 +0100

    Mobile: First version of json from server.

--- mobile/components/screens/FormScreen.js
+++ mobile/components/screens/FormScreen.js
@@ -1,24 +1,56 @@
-import React from 'react';
-import { ScrollView } from 'react-native';
-
-import data from '../../data/form.json';
+import React, { useEffect, useState } from 'react';
+import { ScrollView, View, Text } from 'react-native';
 
 import {Form} from 'react-native-json-forms';
 
+import data from '../../data/form.json';
+import config from '../../extension/config';
 import FormExtension from '../../extension/FormExtension';
 
 
 const FormScreen = props => {
 
+    const [loaded, setLoaded] = useState(null);
+    const [form, setForm] = useState(null);
+
+    useEffect(() => {
+        (async () => {
+            const res = await fetch(`${config.serverURL}/api/surveys/`,{
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                /*body: JSON.stringify({
+                    name: name,
+                    password: password,
+                    email: email,
+                    type: 'normal'
+                })*/
+            });
+    
+            if (res.status == 200){
+                setForm(await res.json());
+                setLoaded(true);
+            }
+            else
+                Alert.alert('ERROR', 'User already registered with that e-mail.');
+        })();
+    }, []);
+
     const onSubmit = (data) => {
         console.log(data);
     };
 
-    return (
-        <ScrollView style={{ width: '100%' }}>
-            <Form json={data} extension={FormExtension} onSubmit={onSubmit} />
-        </ScrollView>
-    );
+    if (loaded === null)
+        return <View><Text>Loading survey...</Text></View>
+    else if (loaded === false)
+        return <View><Text>Unable to load survey.</Text></View>
+    else
+        return (
+            <ScrollView style={{ width: '100%' }}>
+                <Form json={form} extension={FormExtension} onSubmit={onSubmit} />
+            </ScrollView>
+        );
 };
 
 export default FormScreen;
