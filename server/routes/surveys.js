a92400dc397f8a3e62baa79d34e602640873d98c
Miguel Leitão <mleitao27@gmail.com>
2020-04-16 20:00:42 +0100

    Server: First version of json from server.

--- server/routes/surveys.js
+++ server/routes/surveys.js
@@ -2,8 +2,10 @@
 var express = require('express');
 var router = express.Router();
 
+var formData = require('../data/form.json');
+
 router.post('/', async (req, res) => {
-    console.log('surveys');
+    res.status(200).send(formData);
 });
 
 // Export router
