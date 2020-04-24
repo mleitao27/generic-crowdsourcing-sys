a937a47f19fa752ad74f28ddd44daa02198e6c5f
Miguel Leitão <mleitao27@gmail.com>
2020-04-24 23:26:34 +0100

    Server: diffeFeedback fix. Cache in processAnswer.

--- server/extension/diffFeedbacksArray.js
+++ server/extension/diffFeedbacksArray.js
@@ -5,15 +5,15 @@ var diffFeedback3 = require('./diffFeedback3');
 const feedbacks = [
     {
         name: 'diffFeedback1',
-        module: diffFeedback1
+        module: diffFeedback1.diffFeedback1
     },
     {
         name: 'diffFeedback2',
-        module: diffFeedback2
+        module: diffFeedback2.diffFeedback2
     },
     {
         name: 'diffFeedback3',
-        module: diffFeedback3
+        module: diffFeedback3.diffFeedback3
     }
 ];
 
