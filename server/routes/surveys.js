6f112d77f697168b823124ec5ebfc0c3ec924ea5
Miguel Leitão <mleitao27@gmail.com>
2020-04-29 16:35:40 +0100

    Test 1.

--- server/routes/surveys.js
+++ server/routes/surveys.js
@@ -3,7 +3,6 @@ var express = require('express');
 var router = express.Router();
 
 var formData = require('../data/form.json');
-
 const surveyExtension = require('../extension/surveysExtensionM');
 
 router.post('/', async (req, res) => {
