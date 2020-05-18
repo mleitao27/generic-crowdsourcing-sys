cb5ef639712a4eeb3b19bed067a544b61c1ec36f
Miguel Leitão <mleitao27@gmail.com>
2020-05-18 20:28:00 +0100

    Results screen to extension.

--- mobile/components/screens/ResultsScreen.js
+++ mobile/components/screens/ResultsScreen.js
@@ -1,22 +1,10 @@
 import React from 'react';
-import { View } from 'react-native';
 
-
-import globalStyles from '../../constants/globalStyles';
-import Colors from '../../constants/colors';
-
-import CustomButton from '../CustomButton';
+import ResultsScreenExtension from '../../extension/ResultsScreenExtension';
 
 const ResultsScreen = props => {
     return (
-        <View style={globalStyles.screen}>
-            <CustomButton
-                title='Main'
-                onPress={() => props.navigation.navigate({routeName: 'Main'})}
-                backgroundColor={Colors.primary}
-                textColor={Colors.secondary}
-            />
-        </View>
+        <ResultsScreenExtension navigation={props.navigation} />
     );
 };
 
