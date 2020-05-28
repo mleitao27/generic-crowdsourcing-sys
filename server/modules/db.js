c18a820b2f8ca56ddb8aead88409c9badd4ad2c9
mleitao27 <mleitao27@gmail.com>
2020-05-28 09:31:30 +0100

    Server: Added functions to db.

--- server/modules/db.js
+++ server/modules/db.js
@@ -17,6 +17,43 @@ var loadCollection = async (collection) => {
 
     // Return 'collection' passed as arg
     return client.db(db.name).collection(collection);
-}
+};
+
+var insertDocument = async (collectionName, newDocument) => {
+    const collection = await loadCollection(collectionName);
+    const dID = await collection.insertOne(newDocument);
+
+    return dID.ops[0]._id;
+};
+
+var getDocument = async (collectionName, search) => {
+    const collection = await loadCollection(collectionName);
+    return await collection.find(search).toArray();
+};
+
+var updateDocument = async (collectionName, search, newDocument) => {
+    const collection = await loadCollection(collectionName);
+    await collection.updateOne(search, {$set: newDocument});
+};
+
+var deleteDocument = async (collectionName, search) => {
+    const collection = await loadCollection(collectionName);
+    const result = await collection.find(search).toArray();
+    if (result.length > 0)
+        await collection.deleteOne(search);
+};
+
+var deleteAllDocuments = async (collectionName, search) => {
+    const collection = await loadCollection(collectionName);
+    const result = await collection.find(search).toArray();
+    result.map(async r => {
+        await collection.deleteOne(search);
+    });
+};
 
 exports.loadCollection = loadCollection;
+exports.insertDocument = insertDocument;
+exports.getDocument = getDocument;
+exports.updateDocument = updateDocument;
+exports.deleteDocument = deleteDocument;
+exports.deleteAllDocuments = deleteAllDocuments;
